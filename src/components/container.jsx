2b9b4abbff794d91b0af7363dcb8e7e63ecedd11
AllenKirby <130225133+AllenKirby@users.noreply.github.com>
2024-03-21 20:26:30 +0800

    responsive

--- src/components/container.jsx
+++ src/components/container.jsx
@@ -49,20 +49,20 @@ const Container = () => {
         fetchRandomFlag();
     }, [])
   return (
-    <section className="w-1/3 h-auto p-4 shadow-2xl shadow-black rounded-2xl">
+    <section className="md:w-1/3 w-full h-auto p-4 shadow-2xl shadow-black rounded-2xl">
         <div className="w-full h-auto p-3 flex items-center justify-center">
-            <p className="text-cyan-900 font-bold text-4xl">GUESS THE FLAG</p>
+            <p className="text-cyan-900 font-bold md:text-4xl text-2xl">GUESS THE FLAG</p>
         </div>
         <div className="w-full h-auto flex items-center justify-center py-5">
             <img src={flagData.img} alt="flag" className="w-18 h-18"/>
         </div>
         <form action="" onSubmit={(e)=>{e.preventDefault(); checkGuess();}} className="w-full h-auto p-2 text-center">
             <p className={`${resultColor} font-semibold text-xl`}>{resultMsg}</p>
-            <input type="text" onChange={handleInput} required value={flagGuess} placeholder="Enter the Flag Name" className="w-4/5 p-2 bg-transparent outline-0 border-b-2 border-black placeholder-black text-2xl"/>
+            <input type="text" onChange={handleInput} required value={flagGuess} placeholder="Enter the Flag Name" className="w-4/5 p-2 bg-transparent outline-0 border-b-2 border-black placeholder-black md:text-2xl text-lg "/>
             <button type="submit" className="w-4/5 text-2xl text-white font-semibold py-3 my-2 bg-cyan-800 rounded-2xl">Submit</button>
         </form>
         <div className="w-full py-2 flex items-center justify-center">
-        <button onClick={fetchRandomFlag} className="w-3/5 flex items-center justify-center text-2xl text-white font-semibold py-3 bg-cyan-600 rounded-2xl"><GrCycle size={30} /></button>
+            <button onClick={fetchRandomFlag} className="w-3/5 flex items-center justify-center text-2xl text-white font-semibold py-3 bg-cyan-600 rounded-2xl"><GrCycle size={30} /></button>
         </div>
     </section>
   )
